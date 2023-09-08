15c0bf90ac898457f85075bc5398f8e64f9c0ee3
Enzo Soliz <enzosoliz95@gmail.com>
2023-09-08 00:48:01 -0300

    Modulo2, intro node.js y javascript

---
+++ M02-Node-js-JS/variables-y-tipos-datos/tipos-datos.js
@@ -0,0 +1,47 @@
+//tipo de dato number
+
+let edad = 27;
+// number con decimal
+let peso = 77.5;
+// con el operador 'typeof' podemos ver el tipo de dato que es la variable
+console.log(typeof edad, peso);
+
+//tipo de dato STRING
+//el valor del string puede ir con comillas dobles o simples
+
+let nombre = 'Enzo';
+console.log(nombre);
+
+//tipo de dato boolean 
+
+let verdadero = true;
+let falso = false;
+console.log(verdadero, falso);
+
+//tipo de dato NaN
+
+let notANumber = NaN;
+console.log(notANumber);
+
+//tipo de dato null
+
+let nulo = null;
+console.log(nulo);
+
+// tipo de dato undefined
+
+let sinValor;
+console.log(sinValor);
+
+// tipo de dato objeto literal
+
+let persona = {
+    nombre: 'Enzo',
+    edad: 27
+}
+console.log(persona);
+
+// tipo de dato array
+
+let paises = ['Argentina', 'Colombia', 'Venezuela'];
+console.log(paises);
---
+++ M02-Node-js-JS/variables-y-tipos-datos/variables.js
@@ -0,0 +1,17 @@
+//var 
+var nombre = 'Enzo';
+console.log(nombre);
+
+//let
+let numeroPreferido = 10;
+console.log(numeroPreferido);
+
+if (true) {
+    let numeroPreferido = 11;
+    console.log(numeroPreferido);
+}
+
+//const
+
+const apellido = 'Soliz';
+console.log(apellido);
