7baaae6055e366eecfee518b5448b18ffa042926
Enzo Soliz <enzosoliz95@gmail.com>
2023-09-11 23:59:48 -0300

    funciones en Js

--- M02-Node-js-JS/funciones/funciones.js
+++ M02-Node-js-JS/funciones/funciones.js
@@ -1,27 +1,44 @@
 //funciones expresadas
 
-let sumar = function(numeroA, numeroB){
-    return numeroA + numeroB;
-}
+let sumar = function (numeroA, numeroB) {
+  return numeroA + numeroB;
+};
 
-console.log(sumar(7,9));
+console.log(sumar(7, 9));
 
 //funcion declarada
 
-function restar (numeroC, numeroD){
-    return numeroC -numeroD;
+function restar(numeroC, numeroD) {
+  return numeroC - numeroD;
 }
-console.log(restar(10,3));
-
+console.log(restar(10, 3));
 
 //ejemplo scope
 
-let mensaje = 'hola';
+let mensaje = "hola";
 
-function saludar (){
-    let mensaje = 'chau';
-    return mensaje;
+function saludar() {
+  let mensaje = "chau";
+  return mensaje;
 }
 
-console.log(mensaje)
+console.log(mensaje);
 console.log(saludar());
+
+//Arrow functions
+
+let laMitad = (numero) => numero / 2;
+console.log(laMitad(8));
+
+let dividir = (numero1, numero2) => numero1 / numero2;
+console.log(dividir(20, 5));
+
+let tengoQueTrabajar = (dia) => {
+  if (dia == "Sabado" || dia == "Domingo") {
+    return "No tenes que trabajar";
+  } else {
+    return "Si, Enzo tenes que trabajar";
+  }
+};
+
+console.log(tengoQueTrabajar('Lunes'));
