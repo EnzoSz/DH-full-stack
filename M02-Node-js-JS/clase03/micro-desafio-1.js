5b0426c7fe6d320c90163bb576bdf541e36aff4a
Enzo Soliz <enzosoliz95@gmail.com>
2023-09-13 00:11:17 -0300

    modificando micro-desafio-1

--- M02-Node-js-JS/clase03/micro-desafio-1.js
+++ M02-Node-js-JS/clase03/micro-desafio-1.js
@@ -1,34 +1,32 @@
 //declaramos variables
-let nombre = 'Enzo';
-let apellido = 'Soliz';
+let nombre = "Enzo";
+let apellido = "Soliz";
 let edad = 27;
 let numeroTelefono = 2616668928;
 let socio = true;
-let fechaPartido = '25-09-23';
-let horaPartido = '18hs';
+let fechaPartido = "25-09-23";
+let horaPartido = "18hs";
 let numeroSilla = 250;
-let precioBoleto = 4500.00;
+let precioBoleto = 4500.0;
 
 console.log(nombre);
 console.log(apellido);
-console.log('Edad: '+ edad);
-console.log('Numero de telefono: '+ numeroTelefono);
-console.log('socio: '+ (socio ? 'si':'no'));
-console.log('fecha: '+ fechaPartido);
-console.log('hora: '+ horaPartido);
-console.log('numero de silla: '+ numeroSilla);
-console.log('precio: '+ precioBoleto);
+console.log("Edad: " + edad);
+console.log("Numero de telefono: " + numeroTelefono);
+console.log("socio: " + (socio ? "si" : "no"));
+console.log("fecha: " + fechaPartido);
+console.log("hora: " + horaPartido);
+console.log("numero de silla: " + numeroSilla);
+console.log("precio: " + precioBoleto);
 
-console.log(' ');
+console.log(" ");
 
-console.log('Nombre es de tipo: '+(typeof(nombre)));
-console.log('Apellido es de tipo: '+(typeof(apellido)));
-console.log('Edad es de tipo: '+(typeof(edad)));
-console.log(`Numero de telefono es de tipo: ${(typeof(numeroTelefono))}`);
-console.log('Socio es de tipo: '+(typeof(socio)));
-console.log('Fecha es de tipo: '+(typeof(fechaPartido)));
-console.log('Hora es de tipo: '+(typeof(horaPartido)));
-console.log('Numero de silla es de tipo: '+(typeof(numeroSilla)));
-console.log('Precio es de tipo: '+(typeof(precioBoleto)));
-
-
+console.log(`Nombre es de tipo: ${typeof nombre} \n
+Apellido es de tipo: ${typeof apellido} \n
+Edad es de tipo: ${typeof edad} \n
+Numero de telefono es de tipo: ${typeof numeroTelefono} \n
+Socio es de tipo: ${typeof socio} \n
+Fecha es de tipo: ${typeof fechaPartido} \n
+Hora es de tipo: ${typeof horaPartido} \n
+Numero de silla es de tipo: ${typeof numeroSilla} \n
+Precio es de tipo: ${typeof precioBoleto}`);
