fcd9450f67757fa8b6e5565e8507eeb3c68899a2
Enzo Soliz <enzosoliz95@gmail.com>
2023-11-01 23:43:00 -0300

    desafio clase16: terminado

--- M04-Anatomia-de-sitio-web/clase16/src/app.js
+++ M04-Anatomia-de-sitio-web/clase16/src/app.js
@@ -1,1 +1,33 @@
-console.log('Hola mundo');
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath));
+
+app.listen(3016, () => console.log('Servidor corriendo http://localhost:3016'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+  });
+
+app.get("/babbage", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/babbage.html'));
+});
+app.get("/berners-lee", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/berners-lee.html'));
+});
+app.get("/clarke", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/clarke.html'));
+});
+app.get("/hamilton", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/hamilton.html'));
+});
+app.get("/hopper", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/hopper.html'));
+});
+app.get("/lovelace", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/lovelace.html')); 
+});
+app.get("/turing", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/turing.html'));
+});
